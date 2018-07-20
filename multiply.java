eab313738a4dcc1b481d4060617fe85cd2468c2b
MuthurathinamECE <41099238+MuthurathinamECE@users.noreply.github.com>
2018-07-20 15:21:37 +0530

    Add files via upload

---
+++ multiply.java
@@ -0,0 +1,15 @@
+package day20;
+import java.util.Scanner;
+public class multiply {
+public static void main(String[] args) {
+	String a,c="",d="";
+	int b,i;
+	Scanner sc=new Scanner(System.in);
+	b=sc.nextInt();
+	a=sc.next();
+	for(i=0;i<b;i++){
+		System.out.println(a);
+	}
+}
+}
+
