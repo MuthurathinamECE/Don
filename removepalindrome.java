6d86b278d90ad6d776f19435c18edc593dcbfb9e
MuthurathinamECE <41099238+MuthurathinamECE@users.noreply.github.com>
2018-07-31 14:07:15 +0530

    Add files via upload

---
+++ removepalindrome.java
@@ -0,0 +1,29 @@
+package day23;
+import java.util.*;
+public class removepalindrome {
+public static void main(String[] args) {
+	String a,z="";
+	int b=0,c=0,e=0,i,j;
+	Scanner sc=new Scanner(System.in);
+	a=sc.next();
+	b=a.length();
+	char ch[]=new char[b];
+	ch=a.toCharArray();
+	for(i=0;i<b;i++){
+		if(ch[i]!=0){
+		for(j=i+1;j<b;j++){
+			if(ch[j]!=0){
+			if(ch[i]==ch[j]){
+				ch[i]=0;
+				ch[j]=0;
+				c++;
+			}
+			}
+		}
+		}
+}
+	if(c!=0){
+	System.out.println(b-(2*c));
+	}
+}
+}
